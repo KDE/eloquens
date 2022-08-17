31f487679c0074e7f62e2bef029eea54d743a04b
Laurent Montel <montel@kde.org>
2022-08-17 07:03:27 +0200

    fix warning against qt6

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,9 @@ constexpr auto APPLICATION_ID = "org.kde.eloquens";
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
 
