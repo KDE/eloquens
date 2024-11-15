e52ddbf785551d444ca563104335621996662004
Joshua Goins <josh@redstrate.com>
2024-11-14 19:02:12 -0500

    Remove Qt5 code

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,9 @@
 #include <KLocalizedString>
 #include <KDBusService>
 
-#include "controller.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
 
